b917245e5ccd05d2a5b3c14c97c0fe6fc8e1aa81
Deepikatharni <deepika71395@gmail.com>
2016-08-08 17:24:29 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,3 +1,20 @@
-# b
-hjkodd
-ykk
+final static
+{
+public static void main(String args[])
+{
+public int[] merge(final int[] ...arrays ) {
+    int size = 0;
+    for ( int[] a: arrays )
+        size += a.length;
+
+        int[] res = new int[size];
+
+        int destPos = 0;
+        for ( int i = 0; i < arrays.length; i++ ) {
+            if ( i > 0 ) destPos += arrays[i-1].length;
+            int length = arrays[i].length;
+            System.arraycopy(arrays[i], 0, res, destPos, length);
+        }
+
+        return res;
+}
